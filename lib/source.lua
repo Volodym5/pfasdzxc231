9e2c37a07567127436f8296cb1bf20eb339753e5
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2026-06-16 12:58:16 +0300

    Update source.lua

--- lib/source.lua
+++ lib/source.lua
@@ -7076,7 +7076,7 @@ function Library:CreateWindow(info)
             -- Accent left bar on header
             New("Frame", {
                 BackgroundColor3 = "AccentColor",
-                Position = UDim2.fromOffset(-5, 8),
+                Position = UDim2.fromOffset(-7, 7.5),
                 Size     = UDim2.fromOffset(3, 16),
                 ZIndex   = boxHolder.ZIndex + 1,
                 Parent   = header,
